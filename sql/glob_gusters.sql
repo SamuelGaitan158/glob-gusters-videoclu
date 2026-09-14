babe8c981a42e991f1386c2e63aa73fe3cbe4db4
Samuel Andres Gaitan Bastidas <samuelandres0208@gmail.com>
2026-09-14 16:35:37 -0500

    feat:Add database creation and usage statements to SQL script

--- sql/glob_gusters.sql
+++ sql/glob_gusters.sql
@@ -2,3 +2,5 @@
 -- Autor: Samuel Andres Gaitan Bastidas --
 -- fecha: 2024-06-10 --
 
+CREATE DATABASE IF NOT EXISTS `glob_gusters` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
+USE `glob_gusters`;  "gusters": Unknown word.
